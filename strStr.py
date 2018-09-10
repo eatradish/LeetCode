694ba3bd7d03713eb6d0e0d55763215593d4bd0c
eatradish <qingbaodou@gmail.com>
2018-09-11 00:15:09 +0800

    add strStr submissions

---
+++ strStr.py
@@ -0,0 +1,15 @@
+class Solution:
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        if needle not in haystack:
+            return -1
+        if needle == '':
+            return 0
+        for i in range(len(haystack) - len(needle) + 1):
+            if haystack[i:len(needle)+i] == needle:
+                return i
+        return -1
