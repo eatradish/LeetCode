ce1f7cd2b709f05af9229aa30e380e4dd781501a
eatradish <qingbaodou@gmail.com>
2018-09-14 17:41:23 +0800

    add sqrtx

---
+++ sqrtx.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        low = 0
+        high = x
+        while low <= high:
+            mid = (low + high) / 2
+            if mid * mid == x:
+                return int(mid)
+            elif mid * mid < x:
+                low = int(mid + 1)
+            else:
+                high = int(mid - 1)
+        return high
