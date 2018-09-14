d7bc7d18aff2e4857177b4bd3c3eaceb1f96e7dc
eatradish <qingbaodou@gmail.com>
2018-09-14 15:11:59 +0800

    add plus-one

---
+++ plus-one.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        s = ''
+        for i in range(len(digits)):
+            s = s + str(digits[i])
+        num = int(s)
+        num = num + 1
+        l = list(str(num))
+        for i in range(len(l)):
+            l[i] = int(l[i])
+        return l
