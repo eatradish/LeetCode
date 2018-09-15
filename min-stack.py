f14a5d057aa7feaf65810a05abe637f1bf63598a
eatradish <qingbaodou@gmail.com>
2018-09-15 18:44:08 +0800

    add min-stack

---
+++ min-stack.py
@@ -0,0 +1,39 @@
+class MinStack(object):
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack = []
+        self.min = 0
+        
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: void
+        """
+        self.stack = [x] + self.stack
+        self.min = min(self.stack)
+
+    def pop(self):
+        """
+        :rtype: void
+        """
+        del self.stack[0]
+        if self.stack != []:
+            self.min = min(self.stack)
+        
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        return self.stack[0]
+        
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        return self.min
