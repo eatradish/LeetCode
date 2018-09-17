22ac0268b06b5c7a5efeeaa15315e8cb3bbe9e33
eatradish <qingbaodou@gmail.com>
2018-09-18 02:59:52 +0800

    add majority-element

---
+++ majority-element.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        d = {}
+        for i in range(len(nums)):
+            if nums[i] not in d:
+                d[nums[i]] = 1
+            else:
+                d[nums[i]] += 1
+        return sorted(d.items(), key = lambda d:d[1], reverse = True)[0][0]
