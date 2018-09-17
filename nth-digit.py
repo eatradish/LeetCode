629479f29e98200255c95c034f1d2fde645781a4
eatradish <qingbaodou@gmail.com>
2018-09-18 02:42:11 +0800

    add nth-dight

---
+++ nth-digit.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def findNthDigit(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        for i in range(9):
+            d = 9 * 10 ** i
+            if n <= d * (i + 1): 
+                break
+            n -= d * (i + 1)
+        n -= 1
+        return int(str(10**i + n / (i + 1))[n % (i + 1)])
+
+s = Solution().findNthDigit(1)
