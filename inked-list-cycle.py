b36b03634a6149e44a9dca3876144c22b4aae598
eatradish <qingbaodou@gmail.com>
2018-09-22 05:45:48 +0800

    add # Definition for singly-linked list. # class ListNode(object): #     def __init__(self, x): #         self.val = x #         self.next = None from copy import copy class Solution(object):     def hasCycle(self, head):

---
+++ inked-list-cycle.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+from copy import copy
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        if head == None:return False
+        fast = copy(head.next)
+        slow = head
+        while fast:
+            if fast == slow:return True
+            else:
+                fast = fast.next
+                if not fast:return False
+                else:
+                    fast = fast.next
+                    slow = slow.next
+        return False
