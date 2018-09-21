6fb698d1b4f48a2bea50534c13765b1efb31778e
eatradish <qingbaodou@gmail.com>
2018-09-22 06:35:06 +0800

    add import string
    class Solution(object):
        def convertToTitle(self, n):

---
+++ excel-sheet-column-titl.py
@@ -0,0 +1,29 @@
+import string
+
+class Solution(object):
+    def convertToTitle(self, n):
+        """
+        :type n: int
+        :rtype: str
+        """
+        d = {}
+        l = []
+        res = ''
+        n1 = 0
+        n2 = 0
+        a = ''
+        s = string.ascii_uppercase
+        for i in range(len(s)):d[i + 1] = s[i]
+        if n in d:return d[n]
+        if n % 26 == 0:
+            n = n / 26 - 1
+            a ='Z'
+        while n > 26:
+            b = n % 26
+            n = n // 26
+            l.append(b)
+        res = d[n]
+        for i in l[::-1]:res += d[i]
+        res += a
+        return res
+
