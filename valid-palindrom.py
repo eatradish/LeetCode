e2d50706de00cb920354fb8889bd24c4f8ba96ca
eatradish <qingbaodou@gmail.com>
2018-09-22 01:18:42 +0800

    .

--- valid-palindrom.py
+++ valid-palindrom.py
@@ -5,8 +5,7 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        l = list(filter(lambda x: x != ' ', s))
-        a = ''.join(l)
+        a = s
         a = a.lower()
         r = re.sub(r'[^a-zA-Z0-9_]',"", a)
         return r == r[::-1]
