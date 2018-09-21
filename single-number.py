5df34eb8e20e3d3e44ab31b5d9566957ea6f9c04
eatradish <qingbaodou@gmail.com>
2018-09-22 01:56:21 +0800

    add single-number

---
+++ single-number.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        d = {}
+        for i in nums:
+            if i not in d:d[i] = 1
+            else:del d[i]
+        return [i for i in d][0]
