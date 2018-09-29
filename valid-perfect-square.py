ff8ee1d3e84d0bff87c5fd5066e150ea63e6cf11
eatradish <qingbaodou@gmail.com>
2018-09-29 20:54:06 +0800

    add valid-perfect-square

---
+++ valid-perfect-square.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isPerfectSquare(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        return self.mySqrt(num) ** 2 == num
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        if x == 1: return 1
+        low = 0
+        high = x
+        while low <= high:
+            mid = low + (high - low) // 2
+            if mid == x / mid: return mid
+            elif mid < x / mid: low = mid + 1
+            else: high = mid - 1
+        return high
