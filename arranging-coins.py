eb97d7558f04168773616e4dc19480b71ef7b0ce
eatradish <qingbaodou@gmail.com>
2018-10-05 07:34:57 +0800

    add arranging-coins

---
+++ arranging-coins.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def arrangeCoins(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        k = 1
+        while n >= k:
+            n = n - k
+            k += 1
+        return k - 1
