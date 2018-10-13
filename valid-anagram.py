579fa3dba8caa4db2cf31f49e0d2481b057720f8
eatradish <qingbaodou@gmail.com>
2018-10-13 19:58:02 +0800

    修改格式

--- valid-anagram.py
+++ valid-anagram.py
@@ -1,3 +1,4 @@
+from Collections import Counter
 class Solution:
     def isAnagram(self, s, t):
         """
@@ -5,15 +6,6 @@ class Solution:
         :type t: str
         :rtype: bool
         """
-        ds = {}
-        dt = {}
         if len(s) != len(t): return False
-        for i in s:
-            if i not in ds: ds[i] = 1
-            else: ds[i] += 1
-        for i in t:
-            if i not in dt: dt[i] = 1
-            else: dt[i] += 1
-        if ds != dt: return False
-        return True
+        return Counter(s) == Counter(p)
 
